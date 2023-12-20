f02f2755b238d1719bd5797af91499f8b27ce01b
Lea Bugnon <lea.bugnon@heig-vd.ch>
2023-12-20 11:06:26 +0100

    test

--- src/app.svelte
+++ src/app.svelte
@@ -22,10 +22,10 @@
   {#if !$currentGame}
     <section class="jumbotron text-center">
       <div class="container">
-        <h1>Start playing now</h1>
+        <h1>Start playing now !</h1>
         <p class='mt-3'>
-          <button type='button' class="btn btn-success btn-lg" on:click={play('random')}>Easy mode</button>
-          <button type='button' class="btn btn-danger btn-lg" on:click={play('wopr')}>Play against the WOPR</button>
+          <button type='button' class="btn btn-success btn-lg" on:click={play('random')}>Easy mode 😝</button>
+          <button type='button' class="btn btn-danger btn-lg" on:click={play('wopr')}>Play against the WOPR 🔄</button>
         </p>
       </div>
     </section>
@@ -38,8 +38,8 @@
   {#if $currentGame && $currentGame.state !== 'playing'}
     <section class="text-center mt-4">
       <p>
-        <button type='button' class="btn btn-success btn" on:click={play('random')}>Play again in easy mode</button>
-        <button type='button' class="btn btn-danger btn" on:click={play('wopr')}>Play again against the WOPR</button>
+        <button type='button' class="btn btn-success btn" on:click={play('random')}>Play again 🔄 in easy mode</button>
+        <button type='button' class="btn btn-danger btn" on:click={play('wopr')}>Play again 🔄 against the WOPR</button>
       </p>
     </section>
   {/if}
